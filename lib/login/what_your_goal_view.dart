4d27782208324db3d6419cd3587f6facb6ace68f
Het Prajapati <prajapatihet52@gmail.com>
2024-08-12 18:41:29 +0530

    two new files

---
+++ lib/login/what_your_goal_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WhatYourGoalView extends StatefulWidget {
+  const WhatYourGoalView({super.key});
+
+  @override
+  State<WhatYourGoalView> createState() => _WhatYourGoalViewState();
+}
+
+class _WhatYourGoalViewState extends State<WhatYourGoalView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
